72655c72fedf684a673b92bd2979373a5826bbd0
Kevin Patel <patelkevin782002@gmail.com>
2024-10-10 16:12:01 +0530

    Odd/Even/Patter

---
+++ code.js
@@ -0,0 +1,25 @@
+// Even Number
+let arr = [ 2,4,5,3,7,6,8]
+
+for(let i=0; i<arr.length; i++){
+    if(arr[i]%2==0){
+        console.log(arr[i])
+    }
+}
+
+// Odd Number 
+
+let arr1 = [ 2,4,5,3,7,6,8]
+for(let i=0; i<arr1.length; i++){
+    if(arr[i]%2==1){
+        console.log(arr1[i])
+    }
+}
+
+
+// Pattern Printing
+let bag = ""
+for(let i=0; i<5; i++){
+   bag = bag + "*"
+   console.log(bag)
+}
